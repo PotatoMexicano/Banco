f672ac8b86f7d130852b4be93fded0db1a079f09
CESAR GOUVEA <cesar.gouvea@etec.sp.gov.br>
2018-10-31 19:27:19 -0300

    Preenchimento do form Dados do usuario

--- Banco/Dados.cs
+++ Banco/Dados.cs
@@ -21,6 +21,20 @@ namespace Banco
 
         private void Dados_Load(object sender, EventArgs e)
         {
+            label_conta.Text = usuario.Conta.ToString();
+            label_agencia.Text = usuario.Agencia.ToString();
+            label_tipo.Text = usuario.Tipo.ToString();
+            label_senha.Text = usuario.Senha.ToString();
+            label_nome.Text = usuario.Nome.ToString();
+            label_sobrenome.Text = usuario.Sobrenome.ToString();
+            label_cpf.Text = usuario.Cpf.ToString();
+            label_rg.Text = usuario.Rg.ToString();
+            label_sexo.Text = usuario.Sexo.ToString();
+            label_idade.Text = usuario.Idade.ToString();
+
+            
+
+
 
         }
     }
