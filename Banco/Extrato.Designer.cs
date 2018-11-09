77aa6cd5a567e03b2ebaf93c5cb43bfc5bd84323
PotatoMexicano <potatomexicano006@yahoo.com>
2018-11-09 13:47:35 -0200

    Historico de transação

--- Banco/Extrato.Designer.cs
+++ Banco/Extrato.Designer.cs
@@ -31,13 +31,17 @@
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Extrato));
             this.label1 = new System.Windows.Forms.Label();
             this.label_money = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(31, 32);
+            this.label1.Location = new System.Drawing.Point(72, 27);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(130, 22);
             this.label1.TabIndex = 0;
@@ -47,29 +51,49 @@
             // 
             this.label_money.AutoSize = true;
             this.label_money.Font = new System.Drawing.Font("Consolas", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label_money.Location = new System.Drawing.Point(167, 32);
+            this.label_money.Location = new System.Drawing.Point(208, 27);
             this.label_money.Name = "label_money";
             this.label_money.Size = new System.Drawing.Size(60, 22);
             this.label_money.TabIndex = 1;
             this.label_money.Text = "Money";
             // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dataGridView1);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.label_money);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(344, 363);
+            this.panel1.TabIndex = 2;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 68);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(338, 292);
+            this.dataGridView1.TabIndex = 2;
+            // 
             // Extrato
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(323, 92);
-            this.Controls.Add(this.label_money);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(344, 363);
+            this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
-            this.MaximumSize = new System.Drawing.Size(339, 131);
+            this.MaximumSize = new System.Drawing.Size(360, 402);
             this.MinimizeBox = false;
-            this.MinimumSize = new System.Drawing.Size(339, 131);
+            this.MinimumSize = new System.Drawing.Size(360, 402);
             this.Name = "Extrato";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Extrato";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
-            this.PerformLayout();
 
         }
 
@@ -77,5 +101,7 @@
 
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label_money;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
